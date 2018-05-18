19319ef030a302f4346d715610f696615666d1ca
Oliver <oliver.david.norin@gmail.com>
2018-05-18 20:08:31 +0200

    Added javadoc

--- src/main/java/jluhn/Luhn.java
+++ src/main/java/jluhn/Luhn.java
@@ -1,7 +1,20 @@
 package jluhn;
 
+/**
+ * 
+ * This is a java implementation of the Luhn algorithm.
+ * This class can be used to calculate a check digit as well as verifying a number sequence.
+ * 
+ * @author Oliver Norin
+ *
+ */
 public class Luhn {
 	
+	/**
+	 * Verify a number sequence using the Luhn algorithm.
+	 * @param num Number sequence as String. Only numerical characters are allowed. 
+	 * @return true if the number sequence is valid.
+	 */
 	public static boolean verify(String num) {
         int sum = 0;
         for(int i = 0; i < num.length(); i++){
@@ -18,6 +31,12 @@ public class Luhn {
         return sum % 10 == 0;
     }
 
+	/**
+	 * Verify a number sequence using the Luhn algorithm while ignoring certain characters.
+	 * @param num Number sequence as String. 
+	 * @param ignore Regex pattern. Everything that matches this pattern will be removed before verifying.
+	 * @return true if the number sequence is valid.
+	 */
     public static boolean verify(String num, String ignore) {
     	num = num.replaceAll(ignore, "");
         return verify(num);
