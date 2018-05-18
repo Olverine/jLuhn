4bd1c58651e756d3f86fd1f3f3116416043f6634
Oliver <oliver.david.norin@gmail.com>
2018-05-18 20:24:22 +0200

    Removed LuhnException

--- src/main/java/jluhn/LuhnException.java
+++
@@ -1,9 +0,0 @@
-package jluhn;
-
-public class LuhnException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public LuhnException(String msg, Throwable cause){
-		super(msg, cause);
-	}
-}
